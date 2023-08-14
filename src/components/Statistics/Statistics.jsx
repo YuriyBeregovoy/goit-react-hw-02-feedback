47cf2b5dbba7a405dd8ae7fd79e77ed9236a8d4c
YuriyBeregovoy <rik0681@gmail.com>
2023-08-14 20:53:08 +0300

    the step 4 of homework is complete

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,12 @@
 export const Statistics = ({neutral, bad, good, total, positivePercentage}) => {
-  return <div>
+  if (total > 0) {return <div>
     <p>Good: {good}</p>
     <p>Bad: {bad}</p>
     <p>Neutral: {neutral}</p>
     <p>Total: {total}</p>
     <p>Positive feedback: {positivePercentage}%</p>
   </div>
+  } else {
+    return <p>There is no feedback</p>
+  }
 }
