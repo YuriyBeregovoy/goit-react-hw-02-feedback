ec0e753f2fcc1516b406861566a90eefa10ef9a4
YuriyBeregovoy <rik0681@gmail.com>
2023-08-14 11:15:38 +0300

    step 1 of homework is complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,15 +6,38 @@ state = {
   good: 0,
   neutral: 0,
   bad: 0
-}
+  }
+  
+  handleClickGood = () => {
+    this.setState(
+      (prevState) => {
+        return { good: prevState.good + 1, };
+      });
+  };
+
+  handleClickBad = () => {
+    this.setState(
+      (prevState) => {
+        return { bad: prevState.bad + 1, };
+      });
+  };
+
+ handleClickNeutral = () => {
+    this.setState(
+      (prevState) => {
+        return {neutral: prevState.neutral + 1, };
+      });
+  };
+
+
 
   render() {
     return (
      <div> <div>
         <p>Please leave feedback</p>
-        <button>Good</button>
-        <button>Bad</button>
-        <button>Neutral</button>
+        <button onClick={this.handleClickGood}>Good</button>
+        <button onClick={this.handleClickBad}>Bad</button>
+        <button onClick={this.handleClickNeutral}>Neutral</button>
       </div>
       <div>
           <p>Statistics</p>
