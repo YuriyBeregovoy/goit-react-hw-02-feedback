88fb615b663a90a7aa276d3bc8c2978734e58f0c
YuriyBeregovoy <rik0681@gmail.com>
2023-08-14 22:41:16 +0300

    added the Statistics component styles

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,10 +1,12 @@
+import { ItemCount } from "./Statistics.styled"
+
 export const Statistics = ({neutral, bad, good, total, positivePercentage}) => {
   if (total > 0) {return <ul>
     <li>Good: {good}</li>
     <li>Bad: {bad}</li>
     <li>Neutral: {neutral}</li>
-    <li>Total: {total}</li>
-    <li>Positive feedback: {positivePercentage}%</li>
+    <ItemCount>Total: {total}</ItemCount>
+    <ItemCount>Positive feedback: {positivePercentage}%</ItemCount>
   </ul>
   } else {
     return <p>There is no feedback</p>
