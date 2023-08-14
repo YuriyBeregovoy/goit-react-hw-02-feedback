dc7a1683b1ca65b07f50c961395bd8badc1df7e2
YuriyBeregovoy <rik0681@gmail.com>
2023-08-14 12:26:23 +0300

    added the total counter function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,29 +5,42 @@ export class App extends Component {
 state = {
   good: 0,
   neutral: 0,
-  bad: 0
+  bad: 0,
+  total: 0,
+  positiveQuantity: 0
   }
   
   handleClickGood = () => {
     this.setState(
       (prevState) => {
         return { good: prevState.good + 1, };
-      });
+      },   () => this.countTotalFeedback());
   };
 
   handleClickBad = () => {
     this.setState(
       (prevState) => {
         return { bad: prevState.bad + 1, };
-      });
+      },   () => this.countTotalFeedback());
   };
 
  handleClickNeutral = () => {
     this.setState(
       (prevState) => {
         return {neutral: prevState.neutral + 1, };
+      },   () => this.countTotalFeedback()
+    );
+  };
+
+
+  countTotalFeedback = () => {
+    this.setState(
+      (prevState) => {
+        return {total: prevState.neutral + prevState.bad + prevState.good, };
       });
   };
+
+
 
 
 
@@ -44,6 +57,8 @@ state = {
         <p>Good: {this.state.good}</p>
         <p>Bad: {this.state.bad}</p>
         <p>Neutral: {this.state.neutral}</p>
+        <p>Total: {this.state.total}</p>
+        <p>Positive feedback: {this.state.positiveQuantity}%</p>
       </div></div>
   );
  }
